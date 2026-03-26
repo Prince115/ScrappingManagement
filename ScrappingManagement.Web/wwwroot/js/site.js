63b515956e92b7be8547d535012df89e0b8381dd
Deep Bhuva <93590755+DEEPBHUVA@users.noreply.github.com>
2026-03-26 21:07:09 +0530

    Add Dark Mode Support

--- ScrappingManagement.Web/wwwroot/js/site.js
+++ ScrappingManagement.Web/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById('themeToggle').addEventListener('click', function () {
+    const current = document.documentElement.getAttribute('data-bs-theme');
+
+    const next = current === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme', next);
+    localStorage.setItem('theme', next);
+});
